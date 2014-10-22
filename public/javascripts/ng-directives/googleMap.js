d252c34f701306cb73454dac0e9dfc4484c9f50c
Jamie Patel <jamie@notespublication.com>
2014-10-22 15:33:54 +0100

    Added some basic styling

--- public/javascripts/ng-directives/googleMap.js
+++ public/javascripts/ng-directives/googleMap.js
@@ -3,6 +3,8 @@ angular.module('PusherISS').directive('googleMap', function($timeout){
 	return {
 		link: function(scope, el, attrs){
 
+			var marker;
+			
 			var map = new GMaps({
 				div: attrs.id,
 				zoom: 4,
@@ -10,35 +12,26 @@ angular.module('PusherISS').directive('googleMap', function($timeout){
 				lng: 0
 			});
 
-			var marker;
-
-			var image = {
-				url: "/images/iss75.png",
-				// size: new google.maps.Size(50,50),
-			    // origin: new google.maps.Point(100,100),
-			}
-
 			$timeout(function(){
 				map.setCenter(scope.iss.iss_position.latitude, scope.iss.iss_position.longitude)
 				marker = map.addMarker({
 				    lat: scope.iss.iss_position.latitude,
 				    lng: scope.iss.iss_position.longitude,
-					icon: image
+					icon: "/images/iss75.png"
 				});
 			}, 2000)
 
 
 			scope.$watch('path', function(){
-				// map.removeMarkers();
 				var latlng = new google.maps.LatLng(scope.iss.iss_position.latitude, scope.iss.iss_position.longitude);
 				marker.setPosition(latlng);
 
-				// map.drawPolyline({
-				// 	path: scope.path,
-				// 	strokeColor: 'black',
-				// 	strokeOpacity: 1,
-				// 	strokeWeight: 15
-				// });
+				map.drawPolyline({
+					path: scope.path,
+					strokeColor: 'black',
+					strokeOpacity: 0.1,
+					strokeWeight: 5
+				});
 			}, true);
 		}
 	}
